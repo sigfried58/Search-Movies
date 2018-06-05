9a0fe4ef445fdc1e5e014e46088e02cdd840d702
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-06-05 14:35:43 -0400

    Preparing the environment of our app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Title } from './components/Title';
+import './App.css';
+import 'bulma/css/bulma.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Title>Search Movies</Title>
+      </div>
+    );
+  }
+}
+
+export default App;
