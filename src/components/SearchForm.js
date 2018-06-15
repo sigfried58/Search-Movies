7c72c35359a7780202698921171167da362a5d34
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-06-15 13:50:57 -0400

    Using Fetch to get search results from an API

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const API_KEY = '7c60b2c8';
+
 export class SearchForm extends Component {
   state = {
     inputMovie: ''
@@ -11,7 +13,14 @@ export class SearchForm extends Component {
 
   _handleSubmit = e => {
     e.preventDefault();
-    alert(this.state.inputMovie);
+    const { inputMovie } = this.state;
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+      .then(res => res.json())
+      .then(results => {
+        const { Search, totalResults } = results;
+        console.log({ Search, totalResults });
+        this.props.onResults(Search);
+      });
   };
   render() {
     return (
